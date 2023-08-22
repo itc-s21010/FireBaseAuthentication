289ffbe5ac1a86ae5832ec76b36284a0c1ba7a09
s21010 <s21010@std.it-college.ac.jp>
2023-08-23 03:44:04 +0900

    map search

--- app/src/main/java/com/example/firebaseauthentication/MapsActivity.kt
+++ app/src/main/java/com/example/firebaseauthentication/MapsActivity.kt
@@ -2,10 +2,77 @@ package com.example.firebaseauthentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.PopupMenu
+import android.widget.Toast
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 
-class MapsActivity : AppCompatActivity() {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var mGoogleMap:GoogleMap? = null
+    private lateinit var autocompleteFragment:AutocompleteSupportFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        Places.initialize(applicationContext,getString(R.string.google_map_api_key))
+        autocompleteFragment = supportFragmentManager.findFragmentById(R.id.autocomplete_fragment)
+            as AutocompleteSupportFragment
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.ADDRESS, Place.Field.LAT_LNG))
+        autocompleteFragment.setOnPlaceSelectedListener(object :PlaceSelectionListener{
+            override fun onError(p0: Status) {
+                Toast.makeText(this@MapsActivity,"some Error in Search", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onPlaceSelected(place: Place) {
+                val latLng = place.latLng!!
+                zoomOnMap(latLng)
+            }
+        })
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        val mapOptionButton:ImageButton = findViewById(R.id.mapOptionsMenu)
+        val popupMenu = PopupMenu(this,mapOptionButton)
+        popupMenu.menuInflater.inflate(R.menu.map_options,popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener { menuItem ->
+            changeMap(menuItem.itemId)
+            true
+        }
+        mapOptionButton.setOnClickListener {
+            popupMenu.show()
+        }
+    }
+
+    private fun zoomOnMap(latLng:LatLng)
+    {
+        val newLatLngZoom = CameraUpdateFactory.newLatLngZoom(latLng,12f)
+        mGoogleMap?.animateCamera(newLatLngZoom)
+    }
+
+    private fun changeMap(itemId: Int) {
+        when(itemId)
+        {
+            R.id.normal_map -> mGoogleMap?.mapType = GoogleMap.MAP_TYPE_NORMAL
+            R.id.hybrid_map_map -> mGoogleMap?.mapType = GoogleMap.MAP_TYPE_HYBRID
+        }
+    }
+
+
+
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
     }
 }
