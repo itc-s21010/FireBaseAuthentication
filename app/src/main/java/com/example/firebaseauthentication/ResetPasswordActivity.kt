9fca834b7719da04f9de3055d9325376743cb37d
s21010 <s21010@std.it-college.ac.jp>
2023-11-27 13:47:03 +0900

    パスワードリセット機能実装

--- app/src/main/java/com/example/firebaseauthentication/ResetPasswordActivity.kt
+++ app/src/main/java/com/example/firebaseauthentication/ResetPasswordActivity.kt
@@ -2,10 +2,36 @@ package com.example.firebaseauthentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class ResetPasswordActivity : AppCompatActivity() {
+
+    private lateinit var etPassword: EditText
+    private lateinit var btnResetPassword: Button
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reset_password)
+
+        etPassword = findViewById(R.id.et_reset_email)
+        btnResetPassword = findViewById(R.id.btn_reset)
+
+        auth = FirebaseAuth.getInstance()
+
+        btnResetPassword.setOnClickListener {
+            val sPassword = etPassword.text.toString()
+            auth.sendPasswordResetEmail(sPassword)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Please check your Email", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+                }
+        }
     }
 }
